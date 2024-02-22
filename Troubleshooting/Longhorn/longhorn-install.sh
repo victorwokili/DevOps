d5dc7f8748e1b3308c49f9aab60a4fadf4a44ecc
Victor Musa-Wokili <vwokili@gmail.com>
2024-02-22 10:34:30 -0800

    Delete Troubleshooting directory

--- Troubleshooting/Longhorn/longhorn-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo -e "\n
-before installing, ensure nothing longhorn related is running, feel free to use the longhorn-install script\n"
-echo -e "\n\nNow installing\n\n"
-kubectl create namespace longhorn-system
-helm install longhorn longhorn/longhorn --namespace longhorn-system
-echo -e "\n\nInstallation Complete\n"
